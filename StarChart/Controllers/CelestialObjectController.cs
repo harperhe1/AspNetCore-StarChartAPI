7acc974bb701e4c15d09ef3cb489b247634e9e06
harperhe1 <harperhe@hotmail.com>
2022-07-25 15:26:22 -0400

    Added to Controller

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +17,43 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            CelestialObject celestialObject = _context.CelestialObjects.Find(id);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.OrbitedObjectId = celestialObject.Id;
+            
+            return Ok();
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            CelestialObject celestialObject = _context.CelestialObjects.Find(name);
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.OrbitedObjectId = celestialObject.Id;
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<CelestialObject> celestialObjects = _context.CelestialObjects.ToList();
+
+            return Ok();
         }
     }
 }
